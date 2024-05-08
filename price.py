54259103e77cd0d2cdfebb06f8f3e1523b540a4a
smirnovio87 <s89376311108@gmail.com>
2024-05-08 10:57:56 +0300

    Получаем список пар, и список пар только с USDT

--- price.py
+++ price.py
@@ -5,11 +5,29 @@ import json
 # "binance", "bybit", "coinex", "kraken", "bitget", "ascendex", "bequant", "bigone","binanceusdm", "bingx", "bitbns", "bitfinex"
 
 #name_exchanges = ["binance", "bybit", "coinex", "kraken", "bitget", "ascendex", "bequant", "bigone","binanceusdm", "bingx", "bitbns", "bitfinex"]
-name_exchanges = ccxt.exchanges
-name_exchanges = ['bitbank',"binance", "bybit", "coinex"]
+#name_exchanges = ccxt.exchanges
+name_exchanges = ["bitbank", "binance", "bybit", "coinex"]
+exchange_id = "binance"
+market_id="USDT"
+symbol = "BTC/USDT"
 
-symbol = 'BTC/USDT'
+#Получаем список пар
+def get_symbol(exchange_id):
+    exchange_class = getattr(ccxt, exchange_id)
+    exchange= exchange_class()
+    markets = exchange.load_markets()
+    return list(markets.keys())
 
+#Выводим пары только с USDT
+def symbol_usdt(market, maket_id): 
+    usdt_symbol = []
+    for symbol in market:
+        #Проверяем наличие USDT в паре maket_id="USDT"
+        if symbol.endswith(maket_id):
+            usdt_symbol.append(symbol)
+    return usdt_symbol
+print(symbol_usdt(get_symbol(exchange_id), market_id))
+# Получаем курс на биржах и выводим в словарь
 def price_result(name_exchanges):
     exchanges = {}
     for id in name_exchanges:
@@ -36,20 +54,16 @@ def price_result(name_exchanges):
 result = price_result(name_exchanges)
 print("Результат получения ", result)
 
-out_result={}
-for key, value in result.items():
-    if isinstance(value, float):
-        out_result[key]=value
-
-# max(out_result, key=out_result.get)      
-max_value = max(out_result, key=out_result.get)
-min_value = min(out_result, key=out_result.get )
-
-print("Максимальное значение:", max_value, out_result[max_value])
-print("Минимальное значение:", min_value, out_result[min_value])
-
-print("Конечный результат ", out_result)
-# print(sorted(out_result, reverse=True))
+# # Поиск максимального и минимального значения пары на биржах, выводим в словарь
+def max_min(result):
+    out_result={}
+    for key, value in result.items():
+        if isinstance(value, float):
+            out_result[key]=value   
+    max_value = max(out_result, key=result.get)
+    min_value = min(out_result, key=result.get )
+    return {max_value: out_result[max_value], min_value: out_result[min_value]}
+print("Минимальное и максимальное  ", max_min(result))
 
 
 # data_json=price_result(name_exchanges)
